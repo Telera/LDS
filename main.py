1f0ebf291c7226b1c404b7f4f31279c2e054c437
Telera <s.telera@studenti.uniba.it>
2021-10-21 23:44:56 +0200

    First setting

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import pandas as pd
-df = pd.read_csv ('data2021/countries.csv')
-print(df)
+
+df_countries = pd.read_csv ('data2021/countries.csv')
+print(df_countries.info())
+
+df_female = pd.read_csv ('data2021/female_players.csv')
+print(df_female.info())
+
+df_male = pd.read_csv ('data2021/male_players.csv')
+print(df_male.info())
+
+df_tennis = pd.read_csv ('data2021/tennis.csv')
+print(df_tennis.info())
