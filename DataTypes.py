9edc3e5380ed058cf9c4d0e2f5a7676bd1beb729
Telera <s.telera@studenti.uniba.it>
2021-10-22 18:33:39 +0200

    First setting

--- DataTypes.py
+++ DataTypes.py
@@ -1,13 +1,15 @@
 import pandas as pd
+import csv
 
 df_countries = pd.read_csv ('data2021/countries.csv')
-print(df_countries.info())
+#print(df_countries.info())
 
 df_female = pd.read_csv ('data2021/female_players.csv')
-print(df_female.info())
+#print(df_female.info())
 
 df_male = pd.read_csv ('data2021/male_players.csv')
-print(df_male.info())
+#print(df_male.info())
 
 df_tennis = pd.read_csv ('data2021/tennis.csv')
-print(df_tennis.info())
+for name in zip(df_tennis["winner_name"], df_tennis["loser_name"]):
+    print(name)
