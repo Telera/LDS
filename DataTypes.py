446c771ee19bb3f5d8cb1dab60ae25cedc7f678b
Telera <s.telera@studenti.uniba.it>
2021-10-22 16:32:06 +0200

    First setting

--- DataTypes.py
+++ DataTypes.py
@@ -10,4 +10,5 @@ df_male = pd.read_csv ('data2021/male_players.csv')
 print(df_male.info())
 
 df_tennis = pd.read_csv ('data2021/tennis.csv')
-print(df_tennis.info())
+print(df_tennis["loser_age"].sort_values(ascending=False).head(10))
+print(df_tennis["winner_age"].sort_values(ascending=False).head(10))
