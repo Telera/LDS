303cf9ca8a072d12ca464956140efeb2ba4a5032
Telera <s.telera@studenti.uniba.it>
2021-10-26 10:33:19 +0200

    First setting

--- preprocessing.py
+++ preprocessing.py
@@ -35,15 +35,20 @@ def count_max_len_str(path):
         header = f.readline()
         tokens_header = header.strip().split(',')
         len_str = [0] * len(tokens_header)
+        number_flag = [True] * len(tokens_header)
         for line in f:
             if first:
                 first = False
             else:
                 tokens = line.strip().split(',')
                 for ind, token in enumerate(tokens):
-                    if len_str[ind] < len(token):
+                    if not is_number(token):
+                        number_flag[ind] = False
+                    if len_str[ind] < len(token) and not number_flag[ind]:
                         len_str[ind] = len(token)
-        result = list(map(lambda x,y: x + ":" + str(y), tokens_header, len_str))
+                        print(tokens_header[ind], ":", token)
+        #result = list(map(lambda x,y,z: x + ":" + str(y) + str(z), tokens_header, len_str, number_flag))
+        result = list(map(lambda x,y,z: x + ":" + str(y) if not z else x + ": num", tokens_header, len_str, number_flag))
         return(result)
 
 
@@ -62,3 +67,4 @@ print(count_max_len_str("data2021/male_players.csv"))
 print("tennis:")
 print(min_max_columns("data2021/tennis.csv"))
 print(count_max_len_str("data2021/tennis.csv"))
+
