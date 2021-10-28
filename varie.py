9e6f203d25ccae9b11c7822ce209adb18141ff41
giuliamarcoccio <giulia.marcoccio6@gmail.com>
2021-10-28 17:55:20 +0200

    year_of_birth prova

--- varie.py
+++ varie.py
@@ -25,9 +25,40 @@ def split_date(path):
                     quarter = "3"
                 else:
                     quarter = "4"
-                print(tokens[ind_date],day, month, year,quarter )
+                #print(tokens[ind_date],day, month, year,quarter )
 
 split_date("data2021/tennis.csv")
+
+def calculate_year(path):
+    with open("data2021/tennis.csv", "r") as f:
+        header = f.readline()
+        tokens_header = header.strip().split(',')
+        for ind, token in enumerate(tokens_header):
+            if token == "tourney_date":
+                ind_date = ind
+            if token == "winner_age,loser_age":
+                ind_age = ind
+        first = True
+        file = open("output/player.csv", mode='w')
+        file.write("year_of_birth")
+        for line in f:
+            year_of_birth = 0
+            if first:
+                first = False
+            else:
+                tokens = line.strip().split(',')
+                year = int(tokens[ind_date][:4])
+                year_of_birth = year - ind_age
+            #print(year, ind_age, year_of_birth)
+
+
+calculate_year("data2021/tennis.csv")
+
+
+
+
+
+
 
 
 
