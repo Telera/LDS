ecbdd8222e126c7d0553192d821fcc221a679840
Telera <s.telera@studenti.uniba.it>
2022-01-17 01:10:12 +0100

    FINISHED

---
+++ load.py
@@ -0,0 +1,7 @@
+import os
+
+os.system('python load_geography.py')
+os.system('python load_date.py')
+os.system('python load_player.py')
+os.system('python load_tournament.py')
+os.system('python load_match.py')
